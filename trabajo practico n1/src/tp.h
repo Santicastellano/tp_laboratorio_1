8b3d70089b141bb7c8db74a21e2e6745c655aa17
Santicastellano <santiiagocastellano@gmail.com>
2021-04-17 20:54:16 -0300

    listo

--- trabajo practico n1/src/tp.h
+++ trabajo practico n1/src/tp.h
@@ -4,5 +4,21 @@
  *  Created on: 17 abr. 2021
  *      Author: santi
  */
+#ifndef TP_H_INCLUDED
+#define TP_H_INCLUDED
+
+
+int SumarValores(int valorA,int valorB);
+
+int RestarValores(int valorA,int valorB);
+
+int DividirValores(int valorA,int valorB);
+
+int MultiplicarValores(int valorA,int valorB);
+
+long long int HacerFactorial(int valorA);
+
+long long int HacerFactorialB(int valorB);
+#endif // TP_H_INCLUDED
 
 
